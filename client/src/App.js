b3b809c6b63082fbc0e569b7c22cd0f14a29cd1d
Pavol Madar <madar.pavol@gmail.com>
2019-04-04 19:02:04 +0200

    Test S3 cache version 3

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
           <p>
             {this.state.message}
           </p>
-          <p>Version: 2</p>
+          <p>Version: 3</p>
         </header>
       </div>
     );
